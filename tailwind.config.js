316bd88376c69874e841b2d5c7b279d990de7660
J.Thanigaivel <thanigaivel0721@gmail.com>
2024-12-19 00:07:00 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // Ensure the content includes all pages and components for Tailwind to apply styles
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}', // Include all the relevant folders
+    './app/**/*.{js,ts,jsx,tsx,mdx}',  // Ensure all relevant directories are included
   ],
   theme: {
     extend: {
-      // Extend Tailwind with custom styles like gradients, containers, etc.
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
@@ -16,16 +14,16 @@ module.exports = {
       container: {
         center: true,
         padding: {
-          DEFAULT: "1rem",
-          sm: "2rem",
-          lg: "3rem",
-          xl: "4rem",
-          "2xl": "4rem",
-          "3xl": "5rem",
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '3rem',
+          xl: '4rem',
+          '2xl': '4rem',
+          '3xl': '5rem',
         },
       },
       screens: {
-        "4k": "1980px", // Add custom screen breakpoints
+        '4k': '1980px',
       },
     },
   },
