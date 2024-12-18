087b221b8eb4cb477472d613b814684e214beb1d
J.Thanigaivel <thanigaivel0721@gmail.com>
2024-12-19 00:02:28 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    // Ensure the content includes all pages and components for Tailwind to apply styles
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}', // Include all the relevant folders
   ],
   theme: {
     extend: {
+      // Extend Tailwind with custom styles like gradients, containers, etc.
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
@@ -23,7 +25,7 @@ module.exports = {
         },
       },
       screens: {
-        "4k": "1980px",
+        "4k": "1980px", // Add custom screen breakpoints
       },
     },
   },
