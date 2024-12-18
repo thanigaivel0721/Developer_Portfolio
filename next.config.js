a69ee588cfdd4e3b4fac211f09f15ba6012a7590
J.Thanigaivel <thanigaivel0721@gmail.com>
2024-12-19 00:06:33 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  // Set the base path for GitHub Pages if deployed to a subdirectory
-  basePath: '/Developer_Portfolio',  // Replace with your GitHub repository name
-
-  // Add trailingSlash to ensure that your paths end with a slash
+  basePath: '/Developer_Portfolio',  // Ensure this matches your GitHub repository name
   trailingSlash: true,
-
-  // If you're using Image Optimization API, you might need to configure it like this
   images: {
-    unoptimized: true, // Disable Next.js image optimization to prevent issues with static export
+    unoptimized: true,  // Disable image optimization to avoid issues during static export
   },
 }
