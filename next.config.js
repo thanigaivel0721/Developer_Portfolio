a7aefaf4a2bafd0f6ce4d70ac4e9edeaa1bcc549
thanigaivel0721 <thanigaivel0721@gmail.com>
2025-01-19 15:27:08 +0530

    s

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   generateEtags: false,
   headers: async () => [
     {
-      source: '/_next/static/(.*)',
+      source: '/_next/static/(.*)', 
       headers: [
         { key: 'Cache-Control', value: 'no-store, must-revalidate' },
       ],
